ba4177553f88c7ddf5c035c33da36d019bfb3acd
Balogh Bodor Márton <marton2@gmail.com>
2019-12-01 23:10:21 +0100

    F# solution for puzzle 1

---
+++ AdventOfCode.Day01/Library.fs
@@ -0,0 +1,31 @@
+﻿namespace AdventOfCode.Day01
+
+module Puzzles =
+    let calculateMassP1 number =
+        (Operators.floor (number / 3.0f)) - 2.0f
+
+    let rec calculateMassP2 number =
+        let mass = calculateMassP1 number
+        let result = 0
+
+        match mass with
+        | mass when mass <= 0.0f -> 0.0f
+        | _ -> mass + calculateMassP2(mass)
+
+    let puzzle1 numbers =
+        let masses = numbers |> List.map(calculateMassP1)
+        List.sum(masses)
+
+    let puzzle2 numbers =
+        let masses = numbers |> List.map(calculateMassP2)
+        List.sum(masses)
+        
+    let puzzles filename = 
+        let lines = System.IO.File.ReadAllLines(filename)
+        let numbers = lines |> List.ofSeq |> List.map(Operators.float32)
+
+        let res1 = puzzle1 numbers
+        let res2 = puzzle2 numbers
+
+        printfn "Total mass for puzzle 1: %.0f" res1
+        printfn "Total mass for puzzle 2: %.0f" res2
--- AdventOfCode.Runner/Program.cs
+++ AdventOfCode.Runner/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using Microsoft.FSharp.Collections;
 
 namespace AdventOfCode.Runner
 {
@@ -6,7 +11,48 @@ namespace AdventOfCode.Runner
     {
         public static void Main(string[] args)
         {
+            const int solved = 1;
 
+            string header = "Advent of Code 2019";
+            string separator = new string('=', header.Length);
+
+            Console.WriteLine(header);
+            Console.WriteLine(separator);
+            Console.WriteLine();
+
+            int day = args.Length > 0 ? ParseDay(args[0], 0, solved) : 0;
+
+            if (day == 0)
+            {
+                Console.Write("Which day you would like to run? ");
+                
+                string input = Console.ReadLine();
+                day = ParseDay(input, 0, solved);
+            }
+
+            if (day == 0)
+                return;
+
+            AdventOfCode.Day01.Puzzles.puzzles(@"D:\Repositories\AdventOfCode2019\Data\Day01\input.txt");
+
+            Console.WriteLine();
+
+            if (Debugger.IsAttached)
+                return;
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static int ParseDay(string input, int lower, int upper)
+        {
+            if (!int.TryParse(input, out int day))
+                return 0;
+
+            if (day <= lower || day > upper)
+                return 0;
+
+            return day;
         }
     }
 }
